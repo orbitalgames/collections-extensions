551e022b95b817a14955726dad66ab4d43097ca3
William Towns <wtowns@gmail.com>
2015-11-30 16:31:18 -0800

    Add variant of ICollection.WeightedRandom that uses double weights

--- ICollectionExtensions.cs
+++ ICollectionExtensions.cs
@@ -77,5 +77,50 @@ namespace OrbitalGames.Collections
 			}
 			return default(TResult);
 		}
+
+		/// <summary>
+		/// Randomly select a weighted element from the given collection.
+		/// </summary>
+		/// <param name="source">Collection of items from which the weights and elements can be derived</param>
+		/// <param name="weightSelector">A function to extract the weight from an element</param>
+		/// <param name="elementSelector">A function to extract the resulting value from an element</param>
+		/// <param name="rng">Random number generator, or a default new instance if null</param>
+		/// <exception cref="System.ArgumentException">Thrown when <paramref name="source" />, <paramref name="weightSelector" />, or <paramref name="elementSelector" /> is null</exception>
+		/// <returns>Randomly selected weighted element</returns>
+		public static TResult WeightedRandom<TSource, TResult>(this ICollection<TSource> source, Func<TSource, double> weightSelector, Func<TSource, TResult> elementSelector, Random rng = null)
+		{
+			if (source == null)
+			{
+				throw new ArgumentException("source is null", "source");
+			}
+			if (weightSelector == null)
+			{
+				throw new ArgumentException("weightSelector is null", "weightSelector");
+			}
+			if (elementSelector == null)
+			{
+				throw new ArgumentException("elementSelector is null", "elementSelector");
+			}
+			if (source.Count == 0)
+			{
+				throw new InvalidOperationException("source contains no elements");
+			}
+			if (rng == null)
+			{
+				rng = new Random();
+			}
+			double roll = rng.NextDouble() * source.Sum(weightSelector);
+			double currentWeight = 0;
+			var item = source.GetEnumerator();
+			while (item.MoveNext())
+			{
+				currentWeight += weightSelector(item.Current);
+				if (currentWeight >= roll)
+				{
+					return elementSelector(item.Current);
+				}
+			}
+			return default(TResult);
+		}
 	}
 }
