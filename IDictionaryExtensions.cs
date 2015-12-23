65008be95172a37011a8d94455d933782a5c3690
William Towns <wtowns@gmail.com>
2015-12-23 10:58:32 -0800

    Add IDictionaryExtensions class with GetValue extension method

---
+++ IDictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿// IDictionaryExtensions.cs
+// Copyright (c) 2015 Orbital Games, LLC.
+
+using System.Collections.Generic;
+
+namespace OrbitalGames.Collections
+{
+	public static class IDictionaryExtensions
+	{
+		public static TValue GetValue<TKey, TValue>(this IDictionary<TKey, TValue> source, TKey key, TValue defaultValue)
+		{
+			TValue result;
+			return source.TryGetValue(key, out result) ? result : defaultValue;
+		}
+	}
+}
